826c881324e326f7b49737079347c97b997416a1
Arbaaz <arbaazbaig98@gmail.com>
2023-03-06 12:50:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import Landing from './components/Landing';
+import Moreinfo from './components/Moreinfo';
+import Mainsem from './sem1/Mainsem';
+import Subsem1 from './sem1/Subsem1';
+import Subsem2 from './sem2/Subsem2';
+// import Interview from './components/Interview';
+// import AccorM from './questions/AccorM';
+// import CssQ from './questions/CssQ'
+import Footer from './components/Footer';
+// import Mock from './components/Mock';
+import { BrowserRouter as Router,Routes,Route,} from "react-router-dom";
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <Navbar/>
+      
+      <Routes> 
+      <Route path="/" element={<Landing/>} />
+      <Route exact path="/Moreinfo" element={<Moreinfo/>} />
+      <Route exact path="/Sem1" element={<Subsem1/>} />
+      <Route exact path="/Sem2" element={<Subsem2/>} />
+      <Route exact path="/Units" element={<Mainsem/>} />
+      </Routes>
+      {/* <AccorM/> */}
+      <Footer/>
+      {/*  */}
+    </Router>
     </div>
   );
 }
